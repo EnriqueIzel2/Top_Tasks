16ea3a499844af9ca16f016e83df3458c5fbef9e
EnriqueIzel2 <eleaoizel@gmail.com>
2023-03-06 22:02:04 -0400

    feat: add binding

--- app/src/main/java/com/example/toptasks/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/example/toptasks/ui/auth/LoginFragment.kt
@@ -5,14 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.toptasks.R
+import com.example.toptasks.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+  private var _binding: FragmentLoginBinding? = null
+  private val binding get() = _binding!!
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return inflater.inflate(R.layout.fragment_login, container, false)
+    _binding = FragmentLoginBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onDestroyView() {
+    super.onDestroyView()
+    _binding = null
   }
 }
