3a31a40868f5191434176177079ebe0c4ba3f115
EnriqueIzel2 <eleaoizel@gmail.com>
2023-03-10 21:04:46 -0400

    feat: setup tabs with the adapter

--- app/src/main/java/com/example/toptasks/ui/HomeFragment.kt
+++ app/src/main/java/com/example/toptasks/ui/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.toptasks.R
 import com.example.toptasks.databinding.FragmentHomeBinding
+import com.example.toptasks.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
@@ -19,6 +21,27 @@ class HomeFragment : Fragment() {
   ): View {
     _binding = FragmentHomeBinding.inflate(inflater, container, false)
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    initTabs()
+  }
+
+  private fun initTabs() {
+    val viewPageAdapter = ViewPagerAdapter(requireActivity())
+    binding.viewPager.adapter = viewPageAdapter
+
+    viewPageAdapter.addFragment(TodoFragment(), R.string.status_task_todo)
+    viewPageAdapter.addFragment(DoingFragment(), R.string.status_task_doing)
+    viewPageAdapter.addFragment(DoneFragment(), R.string.status_task_done)
+
+    binding.viewPager.offscreenPageLimit = viewPageAdapter.itemCount
+
+    TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+      tab.text = getString(viewPageAdapter.getTitle(position))
+    }.attach()
   }
 
   override fun onDestroy() {
