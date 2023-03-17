30ed2ba9b40e84853d5e71f0215b6847fa737c12
EnriqueIzel2 <eleaoizel@gmail.com>
2023-03-16 21:33:36 -0400

    feat: init the toolbar

--- app/src/main/java/com/example/toptasks/ui/auth/RecoverAccountFragment.kt
+++ app/src/main/java/com/example/toptasks/ui/auth/RecoverAccountFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.toptasks.databinding.FragmentRecoverAccountBinding
+import com.example.toptasks.utils.initToolbar
 
 class RecoverAccountFragment : Fragment() {
 
@@ -19,6 +20,11 @@ class RecoverAccountFragment : Fragment() {
     _binding = FragmentRecoverAccountBinding.inflate(inflater, container, false)
 
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initToolbar(binding.toolbar)
   }
 
   override fun onDestroyView() {
