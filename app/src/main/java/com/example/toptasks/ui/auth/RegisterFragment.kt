3585d410f2799a1820858958042584ab5cbecc43
EnriqueIzel2 <eleaoizel@gmail.com>
2023-03-17 21:32:49 -0400

    feat: create function to init listeners and validate data

--- app/src/main/java/com/example/toptasks/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/example/toptasks/ui/auth/RegisterFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.toptasks.databinding.FragmentRegisterBinding
 import com.example.toptasks.utils.initToolbar
@@ -24,6 +25,28 @@ class RegisterFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     initToolbar(binding.toolbar)
+    initListeners()
+  }
+
+  private fun initListeners() {
+    binding.registerBtnCreateAccount.setOnClickListener {
+      validateData()
+    }
+  }
+
+  private fun validateData() {
+    val email = binding.registerEditTextEmail.text.toString().trim()
+    val password = binding.registerEditTextPassword.text.toString().trim()
+
+    if (email.isNotEmpty()) {
+      if (password.isNotEmpty()) {
+        Toast.makeText(requireContext(), "Certo", Toast.LENGTH_SHORT).show()
+      } else {
+        Toast.makeText(requireContext(), "Preencha uma senha", Toast.LENGTH_SHORT).show()
+      }
+    } else {
+      Toast.makeText(requireContext(), "Preencha um email válido", Toast.LENGTH_SHORT).show()
+    }
   }
 
   override fun onDestroyView() {
