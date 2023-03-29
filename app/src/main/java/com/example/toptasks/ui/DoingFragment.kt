4102eac7d94aa4a65c47af089e42ed96ffb9c714
EnriqueIzel2 <eleaoizel@gmail.com>
2023-03-28 21:09:02 -0400

    feat: create a function to init recyclerView a function to generate fake data

--- app/src/main/java/com/example/toptasks/ui/DoingFragment.kt
+++ app/src/main/java/com/example/toptasks/ui/DoingFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.toptasks.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.toptasks.data.model.Status
+import com.example.toptasks.data.model.Task
 import com.example.toptasks.databinding.FragmentDoingBinding
+import com.example.toptasks.ui.adapter.TaskAdapter
 
 class DoingFragment : Fragment() {
 
   private var _binding: FragmentDoingBinding? = null
   private val binding get() = _binding!!
+
+  private lateinit var taskAdapter: TaskAdapter
 
   override fun onCreateView(
     inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +25,27 @@ class DoingFragment : Fragment() {
     _binding = FragmentDoingBinding.inflate(inflater, container, false)
     return binding.root
   }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    initRecyclerView(generateFakeTasks())
+  }
+
+  private fun initRecyclerView(taskList: List<Task>) {
+    taskAdapter = TaskAdapter(taskList)
+
+    binding.fragmentDoingRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    binding.fragmentDoingRecyclerView.setHasFixedSize(true)
+    binding.fragmentDoingRecyclerView.adapter = taskAdapter
+  }
+
+  private fun generateFakeTasks() = listOf(
+    Task("0", "Criar task", Status.DOING),
+    Task("1", "Alterar uma task", Status.DOING),
+    Task("2", "Comprar pão", Status.DOING),
+    Task("3", "Fazer pudim", Status.DOING),
+  )
 
   override fun onDestroy() {
     super.onDestroy()
