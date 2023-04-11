d1e851b532f53baf4e6b4fd6c0df2f0e36ca8693
EnriqueIzel2 <eleaoizel@gmail.com>
2023-04-10 21:35:36 -0400

    feat: create function to set status indicators

--- app/src/main/java/com/example/toptasks/ui/adapter/TaskAdapter.kt
+++ app/src/main/java/com/example/toptasks/ui/adapter/TaskAdapter.kt
@@ -1,12 +1,18 @@
 package com.example.toptasks.ui.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.example.toptasks.R
+import com.example.toptasks.data.model.Status
 import com.example.toptasks.data.model.Task
 import com.example.toptasks.databinding.ItemTaskBinding
 
 class TaskAdapter(
+  private val context: Context,
   private val taskList: List<Task>
 ) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
@@ -25,6 +31,30 @@ class TaskAdapter(
   override fun onBindViewHolder(holder: ViewHolder, position: Int) {
     val task = taskList[position]
     holder.binding.itemTaskDescription.text = task.description
+
+    setIndicators(task, holder)
+  }
+
+  private fun setIndicators(task: Task, holder: ViewHolder) {
+    when (task.status) {
+      Status.TODO -> {
+        holder.binding.itemTaskIcBack.isVisible = false
+      }
+
+      Status.DOING -> {
+        holder.binding.itemTaskIcBack.setColorFilter(
+          ContextCompat.getColor(context, R.color.status_todo_color)
+        )
+
+        holder.binding.itemTaskIcForward.setColorFilter(
+          ContextCompat.getColor(context, R.color.status_done_color)
+        )
+      }
+
+      Status.DONE -> {
+        holder.binding.itemTaskIcForward.isVisible = false
+      }
+    }
   }
 
   override fun getItemCount() = taskList.size
